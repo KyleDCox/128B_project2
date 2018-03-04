aee77c5563cfec1c2cf6a4d6a74da006b346a71b
steventeddy <stlan@ucdavis.edu>
2018-03-03 22:02:05 -0800

    part_v: add code

---
+++ part_v.m
@@ -0,0 +1,20 @@
+function [weights1, weights] = part_v(NumHidden, NumNeurons)
+%PART_V creates the weights for the first iteration and subsequent ones
+%   uses rand to create matrices of the appropriate size
+%   NumHidden is the number of hidden layers
+%   NumNeurons is the number of neurons in each hidden layer
+
+% make the first weight matrix
+weights1 = rand(784, max(NumNeurons));
+
+% initialize weights matrix to all zeros
+weights = zeros(max(NumNeurons), max(NumNeurons), NumHidden);
+
+% add weights for the appropriate size
+% rows is number of neurons in next layer
+% columns is number of neurons in current layer
+for i=1:NumHidden - 1
+    weights(NumNeurons(i+1),NumNeurons(i),i) = rand(NumNeurons(i+1),NumNeurons(i));
+end
+% we know that output layer has 10 neurons
+weights(10,NumNeurons(size(NumNeurons)), NumHidden) = rand(10, NumNeurons(size(NumNeurons)));
